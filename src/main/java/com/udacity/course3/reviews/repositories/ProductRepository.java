7309a042a5d03b9617d9b3e7547bbe6e86ace752
ginowine <ginowine1@gmail.com>
2020-11-16 11:50:19 +0100

    repository created

--- src/main/java/com/udacity/course3/reviews/repositories/ProductRepository.java
+++ src/main/java/com/udacity/course3/reviews/repositories/ProductRepository.java
@@ -3,6 +3,9 @@ package com.udacity.course3.reviews.repositories;
 import com.udacity.course3.reviews.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ProductRepository extends MongoRepository<Product, String> {
+import java.util.List;
 
+public interface ProductRepository extends MongoRepository<Product, String> {
+    @Override
+    List<Product> findAll();
 }
