5b7ced1b57cadd3f5ab1c174c26f9a3c8730ddc3
ginowine <ginowine1@gmail.com>
2020-12-27 14:01:26 +0100

    creating repository test

--- src/test/java/com/udacity/course3/reviews/ProductRepositoryTests.java
+++ src/test/java/com/udacity/course3/reviews/ProductRepositoryTests.java
@@ -1,11 +1,24 @@
 package com.udacity.course3.reviews;
 
 import com.udacity.course3.reviews.repositories.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 @DataMongoTest
 public class ProductRepositoryTests {
     @Autowired
     private ProductRepository productRepository;
+
+    private final static List<Double> PRODUCT_ID_LIST = Arrays.asList(1.0, 1.2);
+    private static final Random RANDOM = new Random();
+
+    @BeforeEach
+    public void dataSetup(){
+
+    }
 }
