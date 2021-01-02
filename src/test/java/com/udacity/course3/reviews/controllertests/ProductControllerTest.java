7c6612fa4b394495b573d2bfb629452061f06e35
ginowine <ginowine1@gmail.com>
2021-01-02 06:18:49 +0100

    creating product controller test

--- src/test/java/com/udacity/course3/reviews/controllertests/ProductControllerTest.java
+++ src/test/java/com/udacity/course3/reviews/controllertests/ProductControllerTest.java
@@ -1,13 +1,28 @@
 package com.udacity.course3.reviews.controllertests;
 
 import com.udacity.course3.reviews.controller.ProductsController;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = {ProductsController.class})
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProductsController.class)
 public class ProductControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @Test
+    public void getAllProducts() throws Exception{
+        this.mvc.perform(get("/products")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("")))
+    }
 }
