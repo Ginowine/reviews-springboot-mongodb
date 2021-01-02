66d4e1b49bc09f7e73686b8fb4e453e59b14dd1c
ginowine <ginowine1@gmail.com>
2021-01-02 07:26:24 +0100

    creating product controller test

--- src/test/java/com/udacity/course3/reviews/controllertests/ProductControllerTest.java
+++ src/test/java/com/udacity/course3/reviews/controllertests/ProductControllerTest.java
@@ -1,17 +1,22 @@
 package com.udacity.course3.reviews.controllertests;
 
 import com.udacity.course3.reviews.controller.ProductsController;
+import com.udacity.course3.reviews.model.Product;
+import com.udacity.course3.reviews.repositories.ProductRepository;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
-import static org.hamcrest.CoreMatchers.containsString;
+import java.util.Arrays;
+import java.util.List;
+
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProductsController.class)
@@ -20,9 +25,23 @@ public class ProductControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    ProductRepository productRepository;
+
     @Test
     public void getAllProducts() throws Exception{
-        this.mvc.perform(get("/products")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("")))
+        Product product1 = productRepository.save(new Product("1.0", "Jeans Jacket", 100.2));
+        Product product2 = productRepository.save(new Product("2.0", "Refrigerator", 280.2));
+        Product product3 = productRepository.save(new Product("3.0", "Sweat Pants", 156.2));
+
+        List<Product> allProduct = Arrays.asList(product1, product2, product3);
+        given(productRepository.findAll()).willReturn(allProduct);
+
+        mvc.perform(get("/products")
+        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) jsonPath("$[0].productId", is(product1.getProductId())))
+                .andExpect((ResultMatcher) jsonPath("$[0].productName", is(product1.getProductName())));
+
     }
 }
